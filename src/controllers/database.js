090f1be19cd809e96dd9cbebab0020f69d33071d
Cristhian Saavedra <cristhiansaavedra1996@gmail.com>
2023-07-21 01:47:39 -0300

    Update

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -1,5 +1,5 @@
 
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST,
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT,
-    enableKeepAlive: true
+    keepAlive: true
 });
 
 //getConnection
