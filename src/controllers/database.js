26374b2fc7b15b4cc081dbb14f1e691c5195a61f
sistemasaien <127794947+sistemasaien@users.noreply.github.com>
2023-07-21 00:56:31 -0300

    Update database.js

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     keepAlive: true
 });
 
